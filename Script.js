1ebd7300badf03bf36e2dee8fe2ae159c6f738ef
tb-carl <nanaowusucarl@gmail.com>
2025-11-24 13:04:10 +0000

    added delete button js

--- Script.js
+++ Script.js
@@ -23,4 +23,20 @@ checkbox.classList.add("task-checkbox");
 const span = document.createElement("span");
 span.textContent = userInput;
 
+//delete button
+    const deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "x"
+    deleteBtn.classList.add("deleteBtn")
+    li.appendChild(checkbox)
+    li.appendChild(span)
+    li.appendChild(deleteBtn);
+
+    // used to add a new item to the end of a list
+    tasks.appendChild(li)
+
+    message.textContent = ""
+    inputBox.value = ""
+
+    updateCounts();
 })
+
